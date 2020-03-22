e8e1dd26ba0a301ea62e5d1e0e6e681c4fa8d024
Amy <amyrogers390@gmail.com>
2020-03-22 21:38:40 +1300

    Added Day 6 Part One

--- Day 6.py
+++ Day 6.py
@@ -8,18 +8,51 @@ class BinaryTree:
 
     def insert(self, value):
         if self.right_child == None:
-            self.right_child = BinaryTree(value)
+            self.right_child = value
         elif self.left_child == None:
-            self.left_child = BinaryTree(value)
+            self.left_child = value
         else:
-            print("this node already has two children")
+            print("This node already has two children")
+
+    #iterate through the binary tree from the center of mass
+    def countOrbits(self, count=0):
+        global numOrbits
+        numOrbits += count
+        
+        if self.left_child:
+            self.left_child.countOrbits(count+1)
+
+        if self.right_child:
+            self.right_child.countOrbits(count+1)
 
 def getPuzzleInput(file):
     reader = open(file, 'r')
-    lis = [x.strip() for x in reader.read().split('\n')]
-    f.close()
-    return lis
+    lst = [x.strip().split(')') for x in reader.read().split('\n')]
+    reader.close()
+    return lst
 
-nodes = getPuzzleInput('puzzle_input/day6.txt')
+object_list = getPuzzleInput('puzzle_input/day6.txt')
 
-print(nodes)
+dictionary = {}
+
+#create a binary tree using the dictionary to store the objects
+for obj in object_list:
+    base_obj = obj[0]
+    orbiting_obj = obj[1]
+
+    #create base object if it doesn't exist in dict
+    if(base_obj not in dictionary):
+        dictionary[base_obj] = BinaryTree(base_obj)
+    
+    #create orbiting object if it doesn't exist in dict
+    if(orbiting_obj not in dictionary):
+        dictionary[orbiting_obj] = BinaryTree(orbiting_obj)
+        
+    #add orbiting object as a child of base object
+    dictionary[base_obj].insert(dictionary[orbiting_obj])
+
+#part one: find total number of direct and indirect orbits
+numOrbits = 0
+dictionary['COM'].countOrbits()
+    
+print(numOrbits)
