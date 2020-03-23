d053936de4eb99425d305b95a9bb02c3e4143598
Amy <amyrogers390@gmail.com>
2020-03-23 23:01:12 +1300

    Added part 2 to Day 6

--- Day 6.py
+++ Day 6.py
@@ -31,6 +31,16 @@ def getPuzzleInput(file):
     reader.close()
     return lst
 
+
+#return an array of nodes starting from the COM, going to the given value
+def findPathToRoot(value):
+    for node in dictionary.values():
+        if node.left_child == value or node.right_child == value:
+            arr = findPathToRoot(node)
+            arr.append(node)
+            return arr
+    return []
+
 object_list = getPuzzleInput('puzzle_input/day6.txt')
 
 dictionary = {}
@@ -56,3 +66,29 @@ numOrbits = 0
 dictionary['COM'].countOrbits()
     
 print(numOrbits)
+
+#part two: find path between SAN and YOU
+#create "queues" which hold parent nodes going back from the two nodes
+path_back_from_san = findPathToRoot(dictionary['SAN'])
+path_back_from_you = findPathToRoot(dictionary['YOU'])
+
+#find the earliest part where the paths intersect
+for node in reversed(path_back_from_san):
+    if node in path_back_from_you:
+        intersect = node
+        break
+
+#cut out the parts of each path that go back further than the intersect
+index_san = path_back_from_san.index(intersect)
+path_back_from_san = path_back_from_san[index_san:]
+
+#cut out the extra intersect (otherwise will have a duplicate in final list)
+index_you = path_back_from_you.index(intersect) + 1
+#reverse the path since it currently reads forwards from COM
+path_back_from_you = list(reversed(path_back_from_you[index_you:]))
+
+#create the final path
+path_from_you_to_san = path_back_from_you + path_back_from_san
+
+#minus one from result as it includes the orbit 'YOU' is currently in
+print(len(path_from_you_to_san) - 1)
